18d6affd939d1440496dace53607db9105cd7caa
Santiago Garza <santiago.garza@imforza.com>
2017-08-05 19:15:34 -0700

    Added comments of course

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
-var Gpio = require('onoff').Gpio,
-  pir = new Gpio(17, 'in', 'both');
-  
+// Include dependencies.
+var Gpio = require('onoff').Gpio;
+
+// Define Constants.
 const THIRTY_SECONDS = 30 * 1000,
       BUSY           = true,
       VACANT         = false;
       
-var pir_log = [],
+// Initialize vars
+var pir_sensor  = new Gpio(17, 'in', 'both'), // Pin connected to a passive infrared sensor to detect motion.
+    pir_log     = [],
     last_status = VACANT;
   
-  
+// Check logs every 30 seconds to ensure bathroom is vacant before sending out a notification.
 setInterval(function(){
   if( 0 == pir_log.length ){
-    if( BUSY === last_status ){
-      notify( VACANT );
-    }
+    // Only send out notification if the last polled status was busy.
+    if( BUSY === last_status ){ notify( VACANT ) }
     last_status = VACANT;
   }
   else{
     last_status = BUSY;
   }
-  pir_log = [];
+  pir_log = []; // Clear logs and start poll again
 }, THIRTY_SECONDS );   
 
-pir.watch(function(err, value) {
+// Watch pir sensor for motion,
+pir_sensor.watch(function(err, value) {
   if (err) exit();
   
   if(value == 1){
-    if( 0 == pir_log.length && VACANT === last_status){
-      notify(BUSY);
-    }
-    pir_log.push(value)
+    // Only send out notification if the last polled status was Vacant.
+    if( 0 == pir_log.length && VACANT === last_status){ notify(BUSY) }
+    pir_log.push(value) // Send to logs.
   }
 });
 
 console.log('Restroom Attendant at your service.');
 
+/**
+ * Destroy variables gracefully on exit.
+ */
 function exit() {
-  pir.unexport();
+  pir_sensor.unexport();
   process.exit();
 }
 
+/**
+ * Send status notification.
+ *
+ * Currently writes to log, but in future updates this will connect to your favorite notification system.
+ * 
+ * @param  {bool} status The current bathroom status.True for Busy, false for Vacant.
+ */
 function notify( status ){
   what = ( BUSY === status ) ? 'Busy' : 'Vacant';
   console.log( 'Restroom is ' + what );
